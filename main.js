cec63d9c0c8264ea66eda384bfce1bfeb5188ea7
NakamoriMizuki <75844244+NakamoriMizuki@users.noreply.github.com>
2020-12-13 00:50:16 +0900

    omikuji
    練習です

---
+++ main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+{
+ const btn= document.getElementById('btn');
+
+ btn.addEventListener('click', () => {
+   const results = ['大吉','吉','中吉','小吉','末吉','凶',];
+  
+   btn.textContent = results[Math.floor(Math.random() * results.length)];
+   
+   
+ }) ;
+}
